9dc2b6e20fb9f17385278caae140ce6d8b1c9e98
kashifjavaid1 <125742946+kashifjavaid1@users.noreply.github.com>
2023-06-17 09:00:58 -0700

    onclick events

--- script.js
+++ script.js
@@ -701,6 +701,9 @@
 //  console.log(b);
 // //  console.log(a);
  
-  var a="kashif";
-  var b="muhammad";
+  function sub() {
+      document.getElementById("text").innerText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled";
+    }
+   
+    
 
