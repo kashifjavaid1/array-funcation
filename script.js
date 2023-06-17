cfd4d9db6183edb1a501ab909b751c82142f6149
kashifjavaid1 <125742946+kashifjavaid1@users.noreply.github.com>
2023-06-17 10:05:26 -0700

    commit

--- script.js
+++ script.js
@@ -701,9 +701,19 @@
 //  console.log(b);
 // //  console.log(a);
  
-  function sub() {
-      document.getElementById("text").innerText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled";
-    }
+//   function sub() {
+//       document.getElementById("text").innerText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled";
+//     }
    
-    
+    function over() {
+      document.getElementById("para1").className="para1";
+      document.getElementById("para1").style.color="red";
+    }
+    over();
 
+
+function out() {
+      document.getElementById("para1").className="para1";
+      document.getElementById("para1").style.color=""
+} 
+out()
