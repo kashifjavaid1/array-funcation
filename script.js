a53d9008db71b5619f1053d8f5533a726b136189
kashifjavaid1 <125742946+kashifjavaid1@users.noreply.github.com>
2023-06-18 09:40:04 -0700

    commit

--- script.js
+++ script.js
@@ -35,3 +35,36 @@
 // //    var result=str.split(" ");
 // var result=str.lastIndexOf("")
 //    console.log(result);
+
+// if (0) {
+//    console.log("exur");
+// }
+// else{
+//    console.log("non");
+// }
+// var result=2+"2";
+// console.log(typeof(result));
+// var abc="20.522"
+//   Number.abc
+//   console.log(abc);
+
+   // var abc=.065896;
+   // Math.round(abc);
+   // console.log(abc);
+//   var abc="kashif";
+//   var result=abc.lastIndexOf("if");
+//   console.log(result);
+// var array=[];
+// console.log(typeof(array));
+// var age=40;
+// if (age>=60 || age<40 && ) {
+   
+// }
+// var num=1;
+// var result=num++;
+// console.log(num);
+var array=[100]
+array.length
+console.log(array);
+
+
